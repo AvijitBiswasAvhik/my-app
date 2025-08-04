515d4c4148c14516500997d49d4e4b1d2b8c12eb
AvijitBiswasAvhik <avijitbiswasavhik@gmail.com>
2025-08-05 00:27:29 +0600

    add google verification again

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -3,22 +3,21 @@ import React from "react";
 import Form from "./form";
 export const metadata = {
   title: "Contact Us | Lipika",
-  description: "Get in touch with us. Email, phone, and location information included.",
+  description:
+    "Get in touch with us. Email, phone, and location information included.",
 };
 
-
 export default function ContactPage() {
-  
   return (
     <section className="min-h-screen bg-gradient-to-br from-blue-100 to-indigo-200 flex items-center justify-center py-16 px-4">
       <div className="max-w-4xl w-full bg-white shadow-2xl rounded-2xl overflow-hidden grid md:grid-cols-2">
         {/* Info Section */}
         <div className="bg-indigo-600 text-white p-10 flex flex-col justify-between">
           <div>
-            <h2 className="text-3xl font-bold mb-4">Let's Talk</h2>
+            <h2 className="text-3xl font-bold mb-4">Let&apos;s Talk</h2>
             <p className="text-lg">
-              Have questions, suggestions, or just want to say hello? We'd love
-              to hear from you.
+              Have questions, suggestions, or just want to say hello? We&apos;d
+              love to hear from you.
             </p>
           </div>
           <div className="mt-8">
@@ -31,7 +30,7 @@ export default function ContactPage() {
         {/* Form Section */}
         <div className="p-10">
           <h3 className="text-2xl font-semibold mb-6">Contact Us</h3>
-         <Form />
+          <Form />
         </div>
       </div>
     </section>
